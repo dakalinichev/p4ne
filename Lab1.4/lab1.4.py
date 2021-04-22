fae20dfe14475668e333c1b87b4074c17c808b9b
Kalinichev <rekrutrd@gmail.com>
2021-04-23 01:11:57 +0300

    1

--- Lab1.4/lab1.4.py
+++ Lab1.4/lab1.4.py
@@ -6,21 +6,35 @@ import random
 random.seed()
 random.randint(0x0B000000,0xDF000000)
 random.randint(2,5)
-
+"""
+Создаем класс, унаследованный от класса IPv4Network
+dir(IPv4RandomNetwork)
+   
+"""
 class IPv4RandomNetwork(IPv4Network):
     def __init__(self, p_start=0, p_end=32):
         IPv4Network.__init__(self,
                              (random.randint(0x0B000000, 0xDF000000),
-                             random.randint(p_start, p_end), False),
+                             random.randint(p_start, p_end)),
                              strict=False
                              )
+    def regular(self):
+        return self.is_global and not \
+            (self.is_multicast or self.is_link_local or \
+             self.is_loopback or self.is_private or self.is_reserved or self.is_unspecified)
+    def not_regular(self):
+        return self.is_private and \
+            (self.is_multicast or self.is_link_local or \
+             self.is_loopback or self.is_private or self.is_reserved or self.is_unspecified)
+    def key_value(self):
+        return int(self.network_address) + (int(self.netmask) << 32)
 
-class IPv4RandomNetwork(IPv4Network):
-    def __init__(self, p_start=0, p_end=32):
-        IPv4Network.__init__(self,
-                             (random.randint(0x0B000000, 0xDF000000),
-                              random.randint(p_start, p_end), False)
-                             )
+
+for
+
+
+IPv4RandomNetwork(8, 24).not_regular
+IPv4RandomNetwork(8, 24).is_private
 
 random_network = IPv4RandomNetwork(8)
 ipaddress.ip_address(random.randint(0x0B000000,0xDF000000))
